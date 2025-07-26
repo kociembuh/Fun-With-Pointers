1e2b1de01f72c670805ae112d86d1533738a9533
kociembuh <222768708+kociembuh@users.noreply.github.com>
2025-07-25 23:52:11 -1000

    update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -7,9 +7,9 @@ int main() {
     int age = 15;
     int *pAge = &age;
 
-    cout << &age << endl; //
-    cout << pAge << endl; 
-    cout << *pAge << endl;
+    cout << &age << endl; // prints memory address of variable "age"
+    cout << pAge << endl; // prints pointer "pAge" which is the memory address of "age" variable
+    cout << *pAge << endl; // prints dereferenced pointer variable "pAge"
 
     return 0;
 }
